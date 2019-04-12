6dc0bf3d4f53dd47896a9b9a5f7cd17a8d34043f
Vanessa Dillon <vanessadillonmalisha@gmail.com>
2019-04-12 11:43:01 -0500

    added javascript

--- js/script.js
+++ js/script.js
@@ -15,3 +15,20 @@
 //             $('nav').removeClass('black');
 //         }
 //     })
+
+
+
+
+
+// function toggle_visibility(id) {
+//     var e = document.getElementById(id);
+//     if(e.style.display == 'block')
+//        e.style.display = 'none';
+//     else
+//        e.style.display = 'block';
+//  }
+
+function toggle_visibility(id) {
+    var e = document.getElementById(id);
+    e.style.display = ((e.style.display!='none') ? 'none' : 'block');
+    }
