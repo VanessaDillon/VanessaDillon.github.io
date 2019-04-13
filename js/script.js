8642c24811616b5ee19ca4ab213b117ba598411e
Vanessa Dillon <vanessadillonmalisha@gmail.com>
2019-04-13 14:53:25 -0500

    changes

--- js/script.js
+++ js/script.js
@@ -20,15 +20,15 @@
 
 
 
-// function toggle_visibility(id) {
-//     var e = document.getElementById(id);
-//     if(e.style.display == 'block')
-//        e.style.display = 'none';
-//     else
-//        e.style.display = 'block';
-//  }
-
 function toggle_visibility(id) {
     var e = document.getElementById(id);
-    e.style.display = ((e.style.display!='none') ? 'none' : 'block');
-    }
+    if(e.style.display == 'block')
+       e.style.display = 'none';
+    else
+       e.style.display = 'block';
+ }
+
+// function toggle_visibility(id) {
+//     var e = document.getElementById(id);
+//     e.style.display = ((e.style.display !='none') ? 'none' : 'block');
+//     };
